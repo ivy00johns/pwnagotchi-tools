36e65c6ee1eaa14b840eb958014e2df432732af1
your_username <ivy00johns@users.noreply.github.com>
2023-11-28 18:07:11 -0600

    - New comments. - Using variables instead of hardcoded.

--- cli/cli.mjs
+++ cli/cli.mjs
@@ -1,5 +1,3 @@
-// cli.js
-
 import chalk from "chalk";
 import clear from "clear";
 import figlet from "figlet";
@@ -7,19 +5,22 @@ import inquirer from "inquirer";
 import CLI from "clui";
 import fs from "fs";
 import { execSync } from "child_process";
-import clipboardy from 'clipboardy';
+import clipboardy from "clipboardy";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import * as path from "path";
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import * as path from 'path';
-
+// Get the current file and directory names
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Set the project directory
 const projectDirectory = __dirname;
 
+// CLI components
 const Spinner = CLI.Spinner;
 
+// Constants
 const HASH_TYPE = 22000;
 const ABORT_TEMPERATURE = 100;
 const ABORT_WAIT_TIME = 2;
@@ -29,14 +30,10 @@ const LOCAL_OUTPUT_FILE_DIRECTORY = "./hashcat/outputs";
 const LOCAL_RULES_DIRECTORY = "./hashcat/rules";
 const LOCAL_WORLISTS_DIRECTORY = "./hashcat/wordlists";
 
-clear();
-
-console.log(chalk.yellow(figlet.textSync("Pwnagotchi Tooling", {
-	horizontalLayout: "full"
-})));
-
+// Generate a random number
 const randomNumber = Math.floor(Math.random() * 1000);
 
+// Function to get available commands from package.json
 function getAvailableCommands() {
 	const packageJsonPath = "package.json";
 
@@ -59,34 +56,38 @@ function getAvailableCommands() {
 	}
 }
 
+// Function to run a command or copy it to clipboard
 async function runCommandOrCopyToClipboard(command) {
-	const { action } = await inquirer.prompt([
-	  {
-		type: 'list',
-		name: 'action',
-		message: 'Select an action:',
-		choices: ['Execute', 'Copy to Clipboard', 'Cancel'],
-	  },
-	]);
-  
-	if (action === 'Cancel') {
-	  console.log(chalk.yellow('Action canceled.'));
-	  return;
-	}
-  
-	if (action === 'Copy to Clipboard') {
-	  clipboardy.writeSync(command);
-	  console.log(chalk.green('Command copied to clipboard!'));
-	} else if (action === 'Execute') {
-	  console.log(chalk.blue(`Executing command: ${command}`));
-	  try {
-		execSync(command, { stdio: 'inherit' });
-	  } catch (error) {
-		console.error(chalk.red(`Error running command: ${error.message}`));
-	  }
-	}
-  }
+	const {
+		action
+	} = await inquirer.prompt([{
+		type: "list",
+		name: "action",
+		message: "Select an action:",
+		choices: ["Execute", "Copy to Clipboard", "Cancel"],
+	}, ]);
 
+	if (action === "Cancel") {
+		console.log(chalk.yellow("Action canceled."));
+		return;
+	}
+
+	if (action === "Copy to Clipboard") {
+		clipboardy.writeSync(command);
+		console.log(chalk.green("Command copied to clipboard!"));
+	} else if (action === "Execute") {
+		console.log(chalk.blue(`Executing command: ${command}`));
+		try {
+			execSync(command, {
+				stdio: "inherit"
+			});
+		} catch (error) {
+			console.error(chalk.red(`Error running command: ${error.message}`));
+		}
+	}
+}
+
+// Function to run a command using npm
 async function runCommand(command) {
 	try {
 		execSync(`npm run ${command}`, {
@@ -97,6 +98,7 @@ async function runCommand(command) {
 	}
 }
 
+// Main function to handle user input and execute commands
 async function run() {
 	const {
 		commandType
@@ -145,9 +147,9 @@ async function run() {
 		console.log(chalk.blue("You selected Custom Command."));
 
 		// Implement your custom command logic here
-		const hccapxFiles = fs.readdirSync("./handshakes/hccapx");
-		const wordlistFiles = fs.readdirSync('./hashcat/wordlists').filter(file => !file.endsWith('.gitignore'));
-		const rulesFiles = fs.readdirSync('./hashcat/rules').filter(file => !file.endsWith('.gitignore'));
+		const hccapxFiles = fs.readdirSync(LOCAL_HCCAPX_DIRECTORY);
+		const wordlistFiles = fs.readdirSync(LOCAL_WORLISTS_DIRECTORY).filter(file => !file.endsWith(".gitignore"));
+		const rulesFiles = fs.readdirSync(LOCAL_RULES_DIRECTORY).filter(file => !file.endsWith(".gitignore"));
 
 		const {
 			selectedHccapx
@@ -185,19 +187,27 @@ async function run() {
 	}
 }
 
+// Function to generate a custom command based on user selections
 function generateCustomCommand(hccapx, wordlist, rules) {
-	const sessionBaseName = hccapx.replace(/\.hc22000$/, '');
-	const ruleName = rules.replace(/\.txt$/, '');
-	const wordlistName = wordlist.replace(/\.txt$/, '');
+	const sessionBaseName = hccapx.replace(/\.hc22000$/, "");
+	const ruleName = rules.replace(/\.txt$/, "");
+	const wordlistName = wordlist.replace(/\.txt$/, "");
 	const sessionName = `${sessionBaseName}-${randomNumber}`;
 
-	if (wordlist !== "" && rules !== "") {
-		return `hashcat --hash-type=${HASH_TYPE} --attack-mode=0 --session ${sessionName} --hwmon-temp-abort=${ABORT_TEMPERATURE} -w ${ABORT_WAIT_TIME} --potfile-path "${path.join(projectDirectory, "..", LOCAL_POTFILES_DIRECTORY, `${sessionName}-potfile.txt`)}" --outfile="${path.join(projectDirectory, "..", LOCAL_OUTPUT_FILE_DIRECTORY, `${sessionName}-output.txt`)}" "${path.join(projectDirectory, "..", LOCAL_HCCAPX_DIRECTORY, hccapx)}" --rules-file="${path.join(projectDirectory, "..", LOCAL_RULES_DIRECTORY, ruleName)}" -S "${path.join(projectDirectory, "..", LOCAL_WORLISTS_DIRECTORY, wordlistName)}.txt"`;
-	} else if (wordlist !== "" && rules === "") {
-		return `hashcat --hash-type=${HASH_TYPE} --attack-mode=0 --session ${sessionName} --hwmon-temp-abort=${ABORT_TEMPERATURE} -w ${ABORT_WAIT_TIME} --potfile-path "${path.join(projectDirectory, "..", LOCAL_POTFILES_DIRECTORY, `${sessionName}-potfile.txt`)}" --outfile="${path.join(projectDirectory, "..", LOCAL_OUTPUT_FILE_DIRECTORY, `${sessionName}-output.txt`)}" "${path.join(projectDirectory, "..", LOCAL_HCCAPX_DIRECTORY, hccapx)}" --rules-file="${path.join(projectDirectory, "..", LOCAL_RULES_DIRECTORY, ruleName)}"`;
-	} else if (wordlist === "" && rules !== "") {
-		return `hashcat --hash-type=${HASH_TYPE} --attack-mode=0 --session ${sessionName} --hwmon-temp-abort=${ABORT_TEMPERATURE} -w ${ABORT_WAIT_TIME} --potfile-path "${path.join(projectDirectory, "..", LOCAL_POTFILES_DIRECTORY, `${sessionName}-potfile.txt`)}" --outfile="${path.join(projectDirectory, "..", LOCAL_OUTPUT_FILE_DIRECTORY, `${sessionName}-output.txt`)}" "${path.join(projectDirectory, "..", LOCAL_HCCAPX_DIRECTORY, hccapx)}" -S "${path.join(projectDirectory, "..", LOCAL_WORLISTS_DIRECTORY, wordlistName)}.txt"`
+	const wordlistPath = `${path.join(projectDirectory, "..", LOCAL_WORLISTS_DIRECTORY, wordlistName)}.txt`;
+	const rulePath     = `${path.join(projectDirectory, "..", LOCAL_RULES_DIRECTORY, ruleName)}`;
+	const outputPath   = `${path.join(projectDirectory, "..", LOCAL_OUTPUT_FILE_DIRECTORY, `${sessionName}-output.txt`)}`;
+	const potfilePath  = `${path.join(projectDirectory, "..", LOCAL_POTFILES_DIRECTORY, `${sessionName}-potfile.txt`)}`;
+	const hccapxPath   = `${path.join(projectDirectory, "..", LOCAL_HCCAPX_DIRECTORY, hccapx)}`;
+
+	if (wordlist !== "NONE" && rules !== "NONE") {
+		return `hashcat --hash-type=${HASH_TYPE} --attack-mode=0 --session ${sessionName} --hwmon-temp-abort=${ABORT_TEMPERATURE} -w ${ABORT_WAIT_TIME} --potfile-path "${potfilePath}" --outfile="${outputPath}" "${hccapxPath}" --rules-file="${rulePath}" -S "${wordlistPath}"`;
+	} else if (wordlist !== "NONE" && rules === "") {
+		return `hashcat --hash-type=${HASH_TYPE} --attack-mode=0 --session ${sessionName} --hwmon-temp-abort=${ABORT_TEMPERATURE} -w ${ABORT_WAIT_TIME} --potfile-path "${potfilePath}" --outfile="${outputPath}" "${hccapxPath}" --rules-file="${rulePath}"`;
+	} else if (wordlist === "" && rules !== "NONE") {
+		return `hashcat --hash-type=${HASH_TYPE} --attack-mode=0 --session ${sessionName} --hwmon-temp-abort=${ABORT_TEMPERATURE} -w ${ABORT_WAIT_TIME} --potfile-path "${potfilePath}" --outfile="${outputPath}" "${hccapxPath}" -S "${wordlistPath}"`
 	}
 }
 
+// Run the main function
 run();
